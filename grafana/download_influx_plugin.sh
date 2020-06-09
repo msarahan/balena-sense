db62af0667f02ea37bc8ce2b0b1c2f1e1f39a5ac
Michael Sarahan <msarahan@gmail.com>
2020-06-08 19:33:56 -0500

    mkdir grafana plugins?

--- grafana/download_influx_plugin.sh
+++ grafana/download_influx_plugin.sh
@@ -2,4 +2,5 @@
 
 influx_plugin_version="7.0.0"
 wget -O "/tmp/influx_plugin.zip" https://github.com/grafana/influxdb-flux-datasource/releases/download/v${influx_plugin_version}/grafana-influxdb-flux-datasource-${influx_plugin_version}.zip
+mkdir -p /usr/share/grafana/data/plugins
 unzip -d /usr/share/grafana/data/plugins "/tmp/influx_plugin.zip"
