3ce1cd718cbf7f6d413234aa89eeb55912c25570
Michael Sarahan <msarahan@gmail.com>
2020-06-09 22:28:06 -0500

    make influx datasource more amenable to cloud 2

--- grafana/entry.sh
+++ grafana/entry.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
+set -x
 /usr/src/app/api.sh &
+
+fn="/usr/src/app/provisioning/datasources/influxdb-datasource.yml"
+cp $fn.base $fn
+cat <<EOT >>$fn
+  defaultBucket: ${INFLUX_BUCKET:-"balena-sense"}
+  organization: ${INFLUX_USER:-""}
+  url: ${INFLUX_URL:-"http://influxdb:8086"}
+  type: ${INFLUX_PLUGIN_TYPE:-"influxdb"}
+EOT
+
+if [ -n "$INFLUX_TOKEN" ]; then
+  cat <<EOT >>$fn
+  secureJsonData:
+    token: ${INFLUX_TOKEN}
+EOT
+fi
+
 exec grafana-server -homepath /usr/share/grafana
