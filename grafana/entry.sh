ddeda5b10815d2d7cdcd9c1460d8d17f0a99ae94
Michael Sarahan <msarahan@gmail.com>
2020-06-10 22:40:06 -0500

    give up on influx cloud for grafana; still use for influx

--- grafana/entry.sh
+++ grafana/entry.sh
@@ -2,21 +2,28 @@
 set -x
 /usr/src/app/api.sh &
 
-fn="/usr/src/app/provisioning/datasources/influxdb-datasource.yml"
-cp $fn.base $fn
-cat <<EOT >>$fn
-  defaultBucket: ${INFLUX_BUCKET:-""}
-  organization: ${INFLUX_ORG:-""}
-  orgId: ${INFLUX_ORG_ID:-"1"}
-  url: ${INFLUX_URL:-"http://influxdb:8086"}
-  type: ${INFLUX_PLUGIN_TYPE:-"influxdb"}
-EOT
+#fn="/usr/src/app/provisioning/datasources/influxdb-datasource.yml"
+#cp $fn.base $fn
+#cat <<EOT >>$fn
+#  defaultBucket: ${INFLUX_BUCKET:-""}
+#  organization: ${INFLUX_ORG:-""}
+#  orgId: ${INFLUX_ORG_ID:-"1"}
+#  url: ${INFLUX_URL:-"http://influxdb:8086"}
+#  type: ${INFLUX_PLUGIN_TYPE:-"influxdb"}
+#EOT
+#
+#if [ -n "$INFLUX_TOKEN" ]; then
+#  cat <<EOT >>$fn
+#  secureJsonData:
+#    token: ${INFLUX_TOKEN}
+#EOT
+#fi
 
-if [ -n "$INFLUX_TOKEN" ]; then
-  cat <<EOT >>$fn
-  secureJsonData:
-    token: ${INFLUX_TOKEN}
-EOT
+if [ ! -z ${TIMEZONE+x} ]; then
+  echo "${TIMEZONE}" > /etc/timezone
+  dpkg-reconfigure tzdata
+  # should be enough above... but let's do this too, just in case
+  ln -sf /usr/share/zoneinfo/$TIMEZONE /etc/localtime
 fi
 
 exec grafana-server -homepath /usr/share/grafana
